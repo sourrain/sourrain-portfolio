9c797c519b4f7eff4f63d33f34361179fc9a41a1
Yu Sun <sourrainnn@gmail.com>
2021-04-02 22:59:38 +0800

    ok

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,10 @@
     <p class="text-xl w-screen h-40 flex items-center p-3">
       Creative Developer who loves to direct multimedia performances
     </p>
-{{blocks}}
+    <!-- bg -->
+    
     <!-- card -->
-    <div class="bg-blue text-xs flex h-screen w-screen p-5">
+    <div class="bg-blue text-xs flex">
       <card
         class="w-80 p-5"
         v-for="(block, index) of blocks"
@@ -28,7 +29,7 @@ export default {
     const client = Prismic.client( "https://sourrain-site.cdn.prismic.io/api/v2" );
     client.query("").then(function(res){
       console.log(res.results)
-      this.blocks = res.results})
+     return this.blocks = res.results});
       }
   }
 </script>
